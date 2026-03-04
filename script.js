9d39e9a367dfb428b04247222e20e91bb5555b6b
leilan2027-byte <leilan2027@gmail.com>
2026-03-04 18:01:55 +0000

    5y5yw5

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ console.log("Script started");
 
 // Variable to track the number of clicks
 let clicks = 0;
+let clickValue = 1;
 
 // Variable to store the click dispplay h1
 let clickDisplay = document.getElementById("click-display");
+
+//
+levelDisplay = document.getElementById("level-display")
 
 // Set initial text
 clickDisplay.innerText = "Clicks: " + clicks;
@@ -13,17 +17,27 @@ clickDisplay.innerText = "Clicks: " + clicks;
 function handleClick() {
     console.log("click");
 
-    // add one to click count
-    clicks = clicks + 1;
+        // add one to click count
+        clicks = clicks + clickValue;
 
-    // update display
-    clickDisplay.innerText = "Clicks: " + clicks;
+        // update display
+        clickDisplay.innerText = "Clicks: " + clicks;
 
-    // detect if 
-    if (clicks == 10) {
-        alert("Congratulations you reached the next level you now do 2 per click now!");
-        //
-        clicks = clicks + 2;
+        // detect if 
+        if (clicks == 10) {
+        alert("Congratulations you reached the next level you now do 2 per click!");
+        levelDisplay.innerText = "Level 2";
+        clickValue = 2;
+        
     }
+    else if (clicks == 50) {
+       alert("Congratulations you reached the next level you now do 5 per click!");
+        levelDisplay.innerText = "Level 3";
+        clickValue = 5; 
     }
-
+    else if (clicks == 150) {
+       alert("Congratulations you reached the next level you now do 15 per click!");
+        levelDisplay.innerText = "Level 4";
+        clickValue = 15;
+}
+}
